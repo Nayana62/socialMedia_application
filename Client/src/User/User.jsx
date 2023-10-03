1ffd62d0e4bcb6953818a921c3bd952259e58424
N M Nayana <nmnayana62@gmail.com>
2023-10-03 11:31:00 +0530

    fix: create chat

--- Client/src/User/User.jsx
+++ Client/src/User/User.jsx
@@ -5,18 +5,33 @@ import { followUser, unfollowUser } from "../actions/userAction";
 const User = ({ person }) => {
   const publicFolder = process.env.REACT_APP_PUBLIC_FOLDER;
   const { user } = useSelector((state) => state.authReducer.authData);
-  const dispatch = useDispatch()
-  
+  const dispatch = useDispatch();
+
   const [following, setFollowing] = useState(
     person.followers.includes(user._id)
   );
-  
-  const handleFollow = () => {
-    following
-      ? dispatch(unfollowUser(person._id, user))
-      : dispatch(followUser(person._id, user));
+
+  const handleFollow = async () => {
+    if (following) {
+      dispatch(unfollowUser(person._id, user));
+    } else {
+      dispatch(followUser(person._id, user));
+
+      try {
+        // Send a request to create a chat
+        await createChat({
+          senderId: user._id,
+          receiverId: person._id,
+        });
+        console.log("Chat created successfully");
+      } catch (error) {
+        console.error("Error creating chat:", error);
+      }
+    }
+
     setFollowing((prev) => !prev);
   };
+
   return (
     <div className="follower">
       <div>
@@ -35,9 +50,7 @@ const User = ({ person }) => {
         </div>
       </div>
       <button
-        className={
-          following ? "button fc-button unfollow-botton" : "fc-button"
-        }
+        className={following ? "button fc-button unfollow-botton" : "fc-button"}
         onClick={handleFollow}
       >
         {following ? "Unfollow" : "Follow"}
--- Server/Controllers/ChatController.js
+++ Server/Controllers/ChatController.js
@@ -1,10 +1,24 @@
 import ChatModel from "../Models/chatModel.js";
 
 export const createChat = async (req, res) => {
-  const newChat = new ChatModel({
-    members: [req.body.senderId, req.body.receiverId],
-  });
+  const { senderId, receiverId } = req.body;
+
   try {
+    // Check if a chat already exists between sender and receiver
+    const existingChat = await ChatModel.findOne({
+      members: { $all: [senderId, receiverId] },
+    });
+
+    if (existingChat) {
+      // Chat already exists, you can choose to return a message or status code
+      return res.status(400).json({ message: "Chat already exists" });
+    }
+
+    // Create a new chat if it doesn't exist
+    const newChat = new ChatModel({
+      members: [senderId, receiverId],
+    });
+
     const result = await newChat.save();
     res.status(200).json(result);
   } catch (error) {
